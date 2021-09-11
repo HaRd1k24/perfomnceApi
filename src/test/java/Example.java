4b529f272fbe729825dea3ec4124fd6f26b76ae0
Lev Trapeznikov <leva.trapeznikov@mail.ru>
2021-09-11 16:25:17 +0300

    draft

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -1,6 +1,9 @@
+import io.restassured.RestAssured;
 import io.restassured.authentication.OAuthSignature;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,47 +13,81 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 
 public class Example {
+    static String token;
+    static String act;
+    static String mrcu;
+    static String cookie;
+    static String str;
+    static String cookie1;
+    static String cookie2;
 
-    final String token = "33754969b51d40db8adeb5c8068c1b1b";
+    @BeforeEach
+    @DisplayName("Get")
+    void get() {
 
-    @Test
-    @DisplayName("Авторизация")
-    public void getUser() {
-        Map<String, String> map = new HashMap<>();
-        map.put("Login", "lev-trapeznikov");
-        map.put("password", "Eaa4HPpOyu2%");
-
-        Response response = given().auth().oauth2(token)
-                .contentType(ContentType.JSON).accept(ContentType.JSON).log().all()
+        Response response = (Response) given()
                 .when()
-                .post("https://auth.mail.ru/cgi-bin/auth")
-                .then().statusCode(302).log().all().extract().response();
+                .get("https://mail.ru")
+                .then().statusCode(200).extract().response();
+        System.out.println(response.headers().toString());
+        System.out.println(cookie = response.getCookies() + " Cookie");
+    }
 
-        /*String response = given()
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-                .body(map)
+    @Test
+    void Auth() {
+        System.out.println("Авторизация");
+        RestAssured.baseURI = "https://auth.mail.ru".concat("/jsapi/auth");
+        ValidatableResponse response = RestAssured.given()
+                .auth()
+                .basic("leva.trapeznikova@mail.ru", "Oneninenine8")
+                .queryParam("saveauth", "1")
+                .post()
+                .then().statusCode(200);
+
+
+        System.out.println(response.extract().body().asString());
+      /*  str = response.extract().body().path("act");
+        mrcu = response.extract().body().path("mrcu");*/
+
+    }
+
+   /* public void getDelete(String act, String mrcu,String token) {
+        RestAssured.baseURI = "https://e.mail.ru/".concat("sdc?token="); // https://e.mail.ru/sdc?token=8817d8c5db0e4067a2c6d341708bf2ce
+        ValidatableResponse response1 = RestAssured.given()
+                .cookie("act",act, cookie1).cookie("mrcu",mrcu,cookie2)
+                .queryParam("token",token,new Object[0])
                 .when()
-                .post("https://auth.mail.ru/cgi-bin/auth")
-                .then().statusCode(302).log().all().extract().asString();
-        System.out.println(response);*/
-    }
-
-    @Test
-    @DisplayName("Авторизация и подтверждение")
-    public void acceptUser() {
-        given().contentType(ContentType.JSON).accept(ContentType.JSON).when().cookie("9bdc3d9beb2da0bc4ef6f6108afb6592:570440636667416e190d50535a00550c50515101515807575602515702540708000f505f5b0e0409031654475c6e4206")
-                .get("https://e.mail.ru/inbox/")
-                .then().statusCode(200).log().all();
+                .get().then().statusCode(200).log().all();
+        token = response1.extract().body().path("token").toString();
+        System.out.println(response1.extract().cookies().get("act") + " act TOKEN");
+        System.out.println(response1.extract().cookies().get("mrcu") + " mrcu TOKEN");
+        System.out.println(response1.extract().body().path("token").toString() + " Really TOKEN");
+        System.out.println(response1.extract().body().asString());
 
     }
 
-    @Test
-    @DisplayName("Удаление сообщений")
-    public void deleteLetter() {
-        given().when()
-                .delete("https://e.mail.ru/api/v1/threads/move")
-                .then().statusCode(200).log().all();
-    }
+
+    public void Delete(String token){
+        RestAssured.baseURI = "https://e.mail.ru/".concat("api/v1/threads/status/smart?");
+        ValidatableResponse response2 = RestAssured.given().contentType(ContentType.JSON).accept(ContentType.JSON)
+                .cookie("act",act)
+                .cookie("mrcu",mrcu)
+                .queryParam("folder","0")
+                .queryParam("limit","50")
+                .queryParam("filters","{}")
+                .queryParam("last_modified","1631255909")
+                .queryParam("force_custom_thread","true")
+                .queryParam("supported_custom_metathreads","[\"tomyself\"]")
+                .queryParam("offset","0")
+                .queryParam("email","leva.trapeznikova@mail.ru")
+                .queryParam("htmlencoded","false")
+                .queryParam("token",token)
+                .when().post().then().statusCode(200);
+
+
+        System.out.println(response2.extract().body().asString() + "????????304");
+        System.out.println();
+
+    }*/
 
 }
